da8298243e9814bb10c8c8e026b7460ca33866e4
vince-vibin <vincent.sternad@meconema.de>
2023-12-03 21:43:11 +0100

    add Day 03

---
+++ 2023/Day03/solution.py
@@ -0,0 +1,9 @@
+""" https://adventofcode.com/2023/day/3 """
+
+# to tired for this rn
+
+with open("./input.txt", encoding="utf-8") as input_file:
+    solution = 0
+    
+
+print(f"The Solution is: {solution}")
