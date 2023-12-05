4ccc95392aeb90b5b9e8e345baa37555ff5edd61
Vincent <99386370+vince-vibin@users.noreply.github.com>
2023-12-05 17:31:12 +0000

    add Day05

--- 2023/Day05/solution.py
+++ 2023/Day05/solution.py
@@ -1,1 +1,31 @@
+""" https://adventofcode.com/2023/day/5 """
 
+def parse_maps(map):
+    map = map.split(":")[1]
+    map = map.split("\n")
+    map.pop(0)
+    return map
+
+def get_matches(line):
+    line = line.split(" ")
+    dest = line[0]
+    start = line[1]
+    range = line[2]
+
+    
+
+
+solution = 0
+
+input_file = open("./input.txt").read().strip()
+maps = input_file.split("\n\n")
+
+initial_seeds = maps[0]
+initial_seeds = initial_seeds.split(": ")[1].split(" ")
+
+maps.pop(0)
+
+for map in maps:
+    print(parse_maps(map))
+#print(maps)
+print(f"The Solution is: {solution}")
