cddf05c349b8832fae3db8268198da1b24c82f5c
vince-vibin <vincent.sternad@meconema.de>
2023-12-06 18:49:37 +0100

    add Day06

--- 2023/Day06/solution.py
+++ 2023/Day06/solution.py
@@ -1,7 +1,5 @@
 """ https://adventofcode.com/2023/day/6 """
 
-solution = 0
-
 def get_lines():
     with open("./input.txt", encoding="utf-8") as input_file:
         lines = []
@@ -16,9 +14,27 @@ def parse_lists(list):
             parsed_list.append(int(elem))
     return parsed_list
 
+def get_won_multiplied(won):
+    multiplied = 1
+    for won in won:
+        multiplied *= won
+    return multiplied
+
 lines = get_lines()
 
 times = parse_lists(lines[0].split(":")[1].split(" "))
-distance = parse_lists(lines[1].split(":")[1].split(" "))
+record = parse_lists(lines[1].split(":")[1].split(" "))
 
+won = []
+for time in times:
+    won_per_race = 0
+    for hold_ms in list(range(0, time + 1)):
+        ms_remaining = time - hold_ms
+        distance = ms_remaining * hold_ms
+
+        if distance > record[times.index(time)]:
+            won_per_race += 1
+    won.append(won_per_race)
+
+solution = get_won_multiplied(won)
 print(f"The Solution is: {solution}")
