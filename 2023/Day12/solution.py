f430e730b491c73c9b076d467931455a17e41415
vince-vibin <vincent.sternad@meconema.de>
2023-12-12 19:36:32 +0100

    add Day12

---
+++ 2023/Day12/solution.py
@@ -0,0 +1,17 @@
+""" https://adventofcode.com/2023/day/9 """
+
+solution = []
+rows = []
+sizes = []
+
+with open("./input.txt", encoding="utf-8") as input_file:
+    lines = input_file.readlines()
+    lines = [line.rstrip() for line in lines]
+    for line in lines:
+        row, size = line.split(" ")
+        rows.append(row)
+        sizes.append(size)
+
+
+solution = sum(solution)
+print(f"The Solution is: {solution}")
